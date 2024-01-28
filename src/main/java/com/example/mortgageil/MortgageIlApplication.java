09b39243dcda6ce78d3b4804d8af8b33af713a74
hagay <hagayel@std.hac.ac.il>
2024-01-28 19:17:41 +0200

    Initial commit

---
+++ src/main/java/com/example/mortgageil/MortgageIlApplication.java
@@ -0,0 +1,10 @@
+package com.example.mortgageil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageIlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MortgageIlApplication.class, args
---
+++ src/test/java/com/example/mortgageil/MortgageIlApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.mortgageil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageIlApplicationTests {
+
+    @Test
+    void contextLoads() {
