a40a69bd8344427b9a5103fa0339a4066a6ead7d
hagay <hagayel@std.hac.ac.il>
2024-07-09 22:42:15 +0300

    temp for check AmortizationCalculationService

---
+++ src/main/java/com/example/mortgageil/Config/VarConfig.java
@@ -0,0 +1,15 @@
+package com.example.mortgageil.Config;
+
+
+import com.example.mortgageil.Core.Mortgage.AmortizationCalculationService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class VarConfig {
+
+    /*@Bean
+    public AmortizationCalculationService amortizationCalculationService() {
+        return new AmortizationCalculationService();
+    }*/
+}
--- src/main/java/com/example/mortgageil/Core/Mortgage/AmortizationScheduleService.java
+++ src/main/java/com/example/mortgageil/Core/Mortgage/AmortizationScheduleService.java
@@ -22,46 +22,70 @@ public abstract class AmortizationScheduleService implements IPaymentStrategy {
     private MortgagePlan mortgageDetails;
 
     private List<Payment> payments;
-    @Getter
-    private double paymentsAverage;
-    @Getter
-    private double totalInterest;
-    @Getter
-    private double totalPayment;
-    @Getter
-    private double totalPrincipal;
 
 
-
-    public void load(MortgagePlan mortgagePlan) {
+    public AmortizationScheduleService setMortgagePlan(MortgagePlan mortgagePlan) {
         this.mortgageDetails = mortgagePlan;
         this.payments = getPayments();
-        calc();
+        return this;
     }
 
-    public double getMaxPayment(){
+    public double getMaxPayment() {
         return payments.stream().mapToDouble(Payment::getPayment).max().orElse(0);
     }
 
-    public double getFirstPayment(){
+    public double getFirstPayment() {
         return payments.stream().mapToDouble(Payment::getPayment).findFirst().orElse(0);
     }
 
-    public double getCpi(){
-        return futureData.getFutureCpi(mortgageDetails.getDuration());
+    public double getCpi() {
+        switch (mortgageDetails.getType()) {
+            case NON_LINKED_FIXED:
+            case PRIME_RATE:
+            case VARIABLE_EVERY_FIVE_YEARS:
+            case EURO:
+            case VARIABLE_EVERY_TEN_YEARS:
+            case VARIABLE_EVERY_TWO_AND_HALF_YEARS:
+                return 0;
+            case LINKED_FIXED:
+            case ENTITLEMENT:
+            case VARIABLE_EVERY_TEN_YEARS_LINKED:
+            case VARIABLE_EVERY_TWO_AND_HALF_YEARS_LINKED:
+            case VARIABLE_EVERY_FIVE_YEARS_LINKED:
+                return getFutureCpi(mortgageDetails.getDuration());
+            default:
+                throw new IllegalArgumentException("Unexpected value: " + mortgageDetails.getType());
+        }
     }
 
-    public double getForecastedTotalInterest(){
+    public double getAveragePayment() {
+        return payments.stream().mapToDouble(Payment::getPayment).average().orElse(0);
+    }
+
+    public double getTotalPayment() {
+        return payments.stream().mapToDouble(Payment::getPayment).sum();
+    }
+
+    public double getTotalInterest() {
+        return payments.stream().mapToDouble(Payment::getInterestPaid).sum();
+    }
+
+    public double getTotalPrincipal() {
+        return payments.stream().mapToDouble(Payment::getPrincipalPaid).sum();
+    }
+
+
+    public double getForecastedTotalInterest() {
         return FinancialMath
                 .ForecastedTotalInterest(mortgageDetails.getAmount(),
                         payments.stream().mapToDouble(Payment::getPayment).toArray());
     }
 
-    protected double getFutureInterest(int month){
+    protected double getFutureInterest(int month) {
         return futureData.getFutureInterest(mortgageDetails.getType());
     }
 
-    protected double getFutureCpi(int month){
+    protected double getFutureCpi(int month) {
         return futureData.getFutureCpi(month);
     }
 
@@ -69,47 +93,24 @@ public abstract class AmortizationScheduleService implements IPaymentStrategy {
         return mortgageDetails;
     }
 
-    protected double getInterestRate(){
+    protected double getInterestRate() {
         return mortgageDetails.getInterestRate();
     }
 
-    protected double getAmount(){
+    protected double getAmount() {
         return mortgageDetails.getAmount();
     }
 
-    protected int getDuration(){
+    protected int getDuration() {
         return mortgageDetails.getDuration();
     }
 
-    protected double getInterestRatePerMonth(){
+    protected double getInterestRatePerMonth() {
         return mortgageDetails.getInterestRate() / 12 / 100;
     }
 
-    protected LocalDate getStarDate(){
+    protected LocalDate getStarDate() {
         return mortgageDetails.getStartDate();
     }
 
-
-    private void calc(){
-        calcAverage();
-        calcTotalInterest();
-        calcTotalPayment();
-        calcTotalPrincipal();
-    }
-
-    private void calcAverage(){
-        paymentsAverage = payments.stream().mapToDouble(Payment::getPayment).average().orElse(0);
-    }
-
-    private void calcTotalInterest(){
-        totalInterest = payments.stream().mapToDouble(Payment::getInterestPaid).sum();
-    }
-
-    private void calcTotalPayment(){
-        totalPayment = payments.stream().mapToDouble(Payment::getPayment).sum();
-    }
-
-    private void calcTotalPrincipal(){
-        totalPrincipal = payments.stream().mapToDouble(Payment::getPrincipalPaid).sum();
-    }
 }
